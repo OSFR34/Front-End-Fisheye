810abe1c9264859fd53e3ca1d599e7a35869e21f
OSFR34 <selma-ozturk@outlook.com>
2022-02-20 14:45:24 +0100

    transcription html en JS

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,16 +1,32 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, id, city, country, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `assets/images/Sample_Photos/Photographers_ID_Photos/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+        const photographerLink = document.createElement("a");
+        photographerLink.setAttribute("href", `photographer.html?id=${id}`);
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        img.setAttribute("src", picture);
+        photographerLink.append(img);
+        const photographerName = document.createElement( 'h2' );
+        photographerName.textContent = name;
+        const photographerCityAndCountry = document.createElement('p');
+        photographerCityAndCountry.setAttribute("class", "photographer_city_country");
+        photographerCityAndCountry.textContent = `${city}, ${country}`;
+        const photographerDescription = document.createElement("p");
+        photographerDescription.setAttribute("class"," photographer_description");
+        photographerDescription.textContent = tagline;
+        const photographerPrice = document.createElement("p");
+        photographerPrice.setAttribute("class", "photographer_price");
+        photographerPrice.textContent = `${price}$ / jour`
+
+        article.append(photographerLink);
+        article.appendChild(photographerName);
+        article.append(photographerCityAndCountry);
+        article.append(photographerDescription);
+        article.append(photographerPrice);
         return (article);
     }
     return { name, picture, getUserCardDOM }
