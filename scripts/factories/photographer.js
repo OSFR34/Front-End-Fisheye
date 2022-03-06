5415e789663407368e89a2134420fa654a721fda
OSFR34 <selma-ozturk@outlook.com>
2022-03-06 15:54:49 +0100

    refonte index.js

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -4,15 +4,40 @@ function photographerFactory(data) {
     const picture = `assets/images/Sample_Photos/Photographers_ID_Photos/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const photographerLink = document.createElement(a)
+        const article = document.createElement("article");
+        const photographerLink = document.createElement("a");
+        photographerLink.setAttribute("href",`photographer.html?id = ${id}`);
+        const img = document.createElement ("img");
+        img.setAttribute("src", picture);
+        photographerLink.append(img);
+        const photographerName = document.createElement("h2");
+        photographer.textContent = name;
+        const photographerCityAndCountry = document.createElement ("p");
+        photographerCityAndCountry.setAttribute("class", "photographer_city_country");
+        photographerCityAndCountry.textContent = `${city}, ${country}`;
+        const photographerDescription = document.createElement("p");
+        photographerDescription.setAttribute("class", "photographer_description");
+        photographerDescription.textContent = tagline;
+        const photographerPrice = document.createElement("p");
+        photographerPrice.setAttribute("class", "photographer_price")
+        photographerPrice.textContent = `${price}€ / jour`;
 
+         
         article.append(photographerLink);
         article.appendChild(photographerName);
         article.append(photographerCityAndCountry);
-        article.append(photographerDescription);
-        article.append(photographerPrice);
+        article.append(photographerDescription)
+        article.append(photographerPrice)
+        
         return (article);
     }
     return { name, picture, getUserCardDOM }
 }
+
+// function mediaFactory(data){
+//     const { id, photographerId, title, image, likes, date, price } = data;
+
+//     const pictureMedia = `assets/images/Sample_Photos/photographer_ID_Photos/$`
+
+// }
+    
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -67,6 +67,7 @@
         photographers.forEach((photographer) => {
             const photographerModel = photographerFactory(photographer);
             const userCardDOM = photographerModel.getUserCardDOM();
+            
             photographersSection.appendChild(userCardDOM);
         });
     };
