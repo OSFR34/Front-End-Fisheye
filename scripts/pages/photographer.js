7a7ff08be51d79a5a7405878383512057bdf91f9
OSFR34 <selma-ozturk@outlook.com>
2022-04-17 21:58:19 +0200

    ajout commentaires likes

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,157 +1,86 @@
 // L’évènement DOMContentLoaded est émis lorsque le document HTML initial a été complètement chargé et analysé, sans attendre que les feuilles de style, images et sous-documents aient terminé de charger.
+// La addEventListener()méthode attache un gestionnaire d'événements à un élément.
 document.addEventListener("DOMContentLoaded", ()=>{
-
        // On recupere l'id du photographe situee dans l'url
-
        const photographerId = new URL(document.location).searchParams.get("id");
 
-    //  méthode globale fetch() récupérer des données des photographes à travers le fichier.json de manière asynchrone.
-
-    // entre parenthèses après fetch prend un argument qui contient le chemin de la ressource que ns souhaitons récupérer ensuite (=traduit de .then) response de l'API fetch représenre la réponse d'une requête initialisée
+       // méthode globale fetch() récupérer des données des photographes à travers le fichier.json de manière asynchrone.
+      // entre parenthèses après fetch prend un argument qui contient le chemin de la ressource que ns souhaitons récupérer ensuite (=traduit de .then) response de l'API fetch représenre la réponse d'une requête initialisée
 
        fetch("../../data/photographers.json").then((response)=>{
-
              return response.json();
 
        }).then((result)=>{
-
             const photographersInfo = result.photographers;
-
             const photographersMedias = result.media;
-
             let isOpen = false;
-
-            const selectOptions = document.querySelector("#select-block-options");
-            
-            const firstButtonText = document.querySelector("#select-first-option-text");
-            
+            const selectOptions = document.querySelector("#select-block-options");            
+            const firstButtonText = document.querySelector("#select-first-option-text");            
             const optionsButtons = selectOptions.querySelectorAll("button");
-
             const photographerInfoArray = getPhotographerInfo(photographersInfo, photographerId);
-
             const photographerMediasArray = getPhotographerMedias(photographersMedias, photographerId);
-
             displayPhotographerInfo(photographerInfoArray[0]);
-
             displayPhotographerMedia(photographerMediasArray); 
 
 
 // ---------SELECT ECOUTEUR D'EVENEMENT--------------------------
 
             document.querySelector("#select-first-option").addEventListener("click", ()=>{
-              if(isOpen === false){
-              
-                  // On ouvre le faux select
-                  
-                  selectOptions.style.display = "block";
-                  
-                  isOpen = true;
-                  
-                  return handleButtonsOptions();
-              
+              if(isOpen === false){              
+                  // On ouvre le faux select                  
+                  selectOptions.style.display = "block";                  
+                  isOpen = true;                  
+                  return handleButtonsOptions();              
+              }              
+              if(isOpen === true){              
+                  return closeSelect();          
               }
-              
-              if(isOpen === true){
-              
-                  return closeSelect();
-              
-              }
-
             });
 
-            function closeSelect(){
-     
-              // On ferme le faux select
-                      
-              selectOptions.style.display = "none";
-              
-               return isOpen = false;
-                      
-         
+            function closeSelect(){     
+              // On ferme le faux select                      
+              selectOptions.style.display = "none";              
+               return isOpen = false; 
             }
 
-            function handleButtonsOptions(){
-     
-              optionsButtons.forEach((button)=>{
-                 
-                       button.onclick = ()=>{
-         
-                         // Récupération du contenu du bouton cliquer (textContent récupère le contenu de tous les éléments, y compris <script> et <style>)
-                       
-                           const buttonText = button.textContent;
-         
-                         //   j'affecte le texte du button cliqué au premier bouton (remplacement du texte du premier bouton par celui qui est cliqué)
-                         
-                           button.innerHTML = firstButtonText.textContent;
-                             
+            function handleButtonsOptions(){     
+              optionsButtons.forEach((button)=>{                 
+                       button.onclick = ()=>{         
+                          // Récupération du contenu du bouton cliquer (textContent récupère le contenu de tous les éléments, y compris <script> et <style>)                       
+                          const buttonText = button.textContent;         
+                         // j'affecte le texte du button cliqué au premier bouton (remplacement du texte du premier bouton par celui qui est cliqué)
+                          button.textContent = firstButtonText.textContent;                             
                          //   j'affecte le texte du premier button par celui qui a été cliqué
-                           firstButtonText.innerHTML = buttonText;  
-                           
-                         // je retourne la fct de fermeture de la liste déroulante.
-    
-                          displayPhotographerMedia(filterMedias(buttonText, photographerMediasArray));
-                             
-                          return closeSelect();
-                 
-                       };
-                 
-                 });
-         
-         
+                          firstButtonText.textContent = buttonText;                           
+                         // je retourne la fct de fermeture de la liste déroulante.    
+                          displayPhotographerMedia(filterMedias(buttonText, photographerMediasArray));                             
+                              return closeSelect();                 
+                       };   
+              });
             }
-            // La portée de la constante photographerMediasArray étant limitée, 
-            // je place mon écouteur d'événement ici pour ne pas multiplier les lignes de codes.
-
-            // Mise en place d'écouteur d'événement pour TRIER en utilisant CHANGE et EVENT en paramètre.
-            // A chaque changement de selection,il y a récupération de la valeur CHANGE (donc soit popularité, date, ou titre).
-
-            // document.querySelector()addEventListener("change", (event)=>{
-
-            // // 1er test:  [console.log (event.target.value)]pour vérifier la récupération des valeurs de CHANGE (date, titre, popularité); si ok 
-            // // J'appelle le fct displayPhotographerMedia.
-            // // et je lui met en paramètre une nouvelle fct filterMedias que j'ai créé ds le dossier utils,pour plus de lisibilité.
-            // // je lui mets comme paramètres les mêmes que ds le console.log et j'ajoute en paramètre le tableau des médias 
-            // // = affichage du tableau des médias selon le TRIE de la valeur de CHANGE.
-          
-            //     displayPhotographerMedia(filterMedias(event.target.value, photographerMediasArray));
-    
-            // });
 
             
         // methode cath pour capturer les erreurs 
        }).catch((error)=>{
-
             console.log(error)
-
        });
 
-
        function getPhotographerInfo(photographersArray, idPhotograph){
-
               return photographersArray.filter((photographer)=>{
-
                      return Number(photographer.id) === Number(idPhotograph);
-
               });
-
        }
-// ---Affichage des info du photographe avec comme paramètre (photographerInfos).
+        // Affichage des info du photographe avec comme paramètre (photographerInfos).
        function displayPhotographerInfo(photographerInfos){
-            // création d'une constante qui va créer du code comme en html mais en remplacant les données variables par les cléfs correspondantes à l'objet de la page.
-             const photographerHTML = `
-             
+      // création d'une constante qui va créer du code comme en html mais en remplacant les données variables par les cléfs correspondantes à l'objet de la page.
+            const photographerHTML = `             
                     <div class="box">
                         <h2>${photographerInfos.name}</h2>
                         <p class="photographer_city_country">
-                        <span>
-                            ${photographerInfos.country},
-                        </span>
-                        <span>
-                        ${photographerInfos.city}
-                        </span> 
+                          <span>${photographerInfos.country}</span>
+                          <span>${photographerInfos.city}</span> 
                         </p>
-                        <p class="photographer_description">
-                            ${photographerInfos.tagline}
+                        <p class="photographer_description">${photographerInfos.tagline}
                         </p>
                     </div>
                     <div>
@@ -159,29 +88,24 @@ document.addEventListener("DOMContentLoaded", ()=>{
                     </div>     
                     <div>
                         <a href= "#">
-                        <img src=assets/images/Sample_Photos/Photographers_ID_Photos/${photographerInfos.portrait} />
+                        <img src=assets/images/Sample_Photos/Photographers_ID_Photos/${photographerInfos.portrait}/>
                         </a>
                     </div>
-             
-             `;
-
-              //  injection dans le dom du résultat de photographerHTML, au niveau de la class photographer-header.
+            `;
+              // injection dans le dom du résultat de photographerHTML, au niveau de la class photographer-header.
               document.querySelector(".photographer-header").innerHTML = photographerHTML;
-            //   affiche le nom du photographe dans le titre de la modale
+              // affiche le nom du photographe dans le titre de la modale
               document.querySelector("#namePhotographer").textContent = photographerInfos.name;
-
-              document.querySelector("#photographer-price-day").textContent = `${photographerInfos.price}$ / Jour`;
-
+              // affiche le tarif par photographe dans l'encart total likes
+              document.querySelector("#photographer-price-day").textContent = `${photographerInfos.price}€ / jour`;
        }
-
+      //  Obtenir les médias des photographes  par id du photographe ds le tableau des médias 
        function getPhotographerMedias(mediasArray, idPhotograph){
-
+            // retourne un tableau des médias filtré par média
             return mediasArray.filter((media)=>{
-
+                // retourne le nombre de médias ayant id du photographe et ce nombre est strictement égale au numéro de l'id du photographe
                 return Number(media.photographerId) === Number(idPhotograph);
-
             });
-
        }
 // Affichage de tous les médias par photographe avec comme paramètre le tableau des médias
        function displayPhotographerMedia(arrayMedias){
@@ -189,23 +113,23 @@ document.addEventListener("DOMContentLoaded", ()=>{
               let htmlMedias = "";
             // récupération de chaque média ds le tableau des médias
              arrayMedias.forEach((media)=>{
-                    // htmlMediasnpermet la création du html/+= l'opérateur addition et égale permet la concaténation de la variable arrayMedias (qui récupére chaque média) AVEC le contenu des bactiques.(=étant ici l'assignation)
-                  htmlMedias += `
-                  
+                    // htmlMedias permet la création du html/+= l'opérateur addition et égale permet la concaténation de la variable arrayMedias (qui récupére chaque média) AVEC le contenu des bactiques.(=étant ici l'assignation)
+                  htmlMedias += `                  
                     <div class="gallery-element">        
                         ${mediaFactory(media)}
                         <div class="description-element">
                         <span class="picture-name">${media.title}</span>
-        
-                        <span class="likes-area"><span class="likes-number">${media.likes}</span><i class="far fa-heart"></i></span>
-                        <span id="likes-area-empty"><i class="fas fa-heart"></i></span>
+                        <span class="likes-area" aria-hidden="false" aria-label="likes">
+                          <span class="likes-number">${media.likes}</span>
+                        <i class="fas fa-heart"></i></span>
+                        
                     </div>        
                     </div>
                   
                   `;
 
              });
-            //  injection dans le dom du résultat de htmlMedias, au niveau de l'id gallery.
+            //  injection dans le dom du contenu de la variable htmlMedias (grâce à la methode .innerHTML qui renvoi le contenu textuel ainsi que les balises et attributs), au niveau de l'id gallery.
              document.querySelector("#gallery").innerHTML = htmlMedias;
 
     //------------------AJOUTS POUR CREER LA LIGHTBOX------------------------------//
@@ -228,22 +152,22 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
                   });
 
-             });
+             }); 
 
+    /* --------------- TOTAL LIKES -----------------*/
+
+    // initialisation de la variable à 0.
              let totalNumberOfLikes = 0;
-
+/*syntaxe for (initialisation; condition: la boucle sera exécuté tant que i sera inférieur à la longueur du tableau des médias;incrémentation +1 : À chaque éxécution, la variable "i" augmentera de 1.Lorsque'elle sera arrivée à la longueur du tableau, le boucle se terminera.*/
              for(let i=0; i<arrayMedias.length; i++){
-        
+                  // affectation après addition de la somme des itérations des likes du tableau des médias en valeur number.
                   totalNumberOfLikes+= Number(arrayMedias[i].likes);
-
              }
-
+            //  injection du résultat (totalNumberOfLikes) à l'emplacement de l'id #photographer-all-likes
              document.querySelector("#photographer-all-likes").textContent = totalNumberOfLikes;
-
+            //  exécution de la fonction likes
              likes();
        }
-
-
 });
 
 
